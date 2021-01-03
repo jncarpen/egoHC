8ca27b0c0d848d6d2c91db0432e5613bd4c7cbb6
Jo Carpenter <jordannc@stud.ntnu.no>
2021-01-03 10:28:56 -0500

    save

--- forIda/populationSpikes.m
+++ forIda/populationSpikes.m
@@ -39,3 +39,9 @@ end
 % transpose the spikes matrix
 popSpikes = spikesMat';
 
+% save the matrix
+savepath = 'ida\something';
+savename = 'popSpikes_session1.mat';
+savestring = strcat(savepath, '\', savename);
+save(savestring, popSpikes, '-v7.3')
+
