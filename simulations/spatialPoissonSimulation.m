1b659cd2c6f5dbbc9ff0b673e138d8b8ce3ae619
Jo Carpenter <jordannc@stud.ntnu.no>
2021-01-15 08:26:00 -0500

    replicate unit

--- simulations/spatialPoissonSimulation.m
+++ simulations/spatialPoissonSimulation.m
@@ -79,3 +79,113 @@ end
 
 end
 
+
+%% make a ratemap
+
+% x/y pos
+xpos = P(:,2);
+ypos = P(:,3);
+
+% bin the arena and find bin centers
+nBins = 100;
+xEdges = linspace(0,150,nBins+1);
+yEdges = linspace(0,150,nBins+1);
+xCenter = (diff(xEdges)./2)+xEdges(1:end-1);
+yCenter = (diff(yEdges)./2)+yEdges(1:end-1);
+[X,Y] = meshgrid(xCenter,yCenter);
+
+% get variance
+xvar = nanvar(xpos); 
+yvar = nanvar(ypos); 
+
+% get standard deviation (sigma)
+xsd = sqrt(xvar); 
+ysd = sqrt(yvar);
+
+% kendall or pearson correlation (excludes NaNs)
+warning('off','all')
+rho = corr(xpos,ypos,'Type','Pearson','Rows','complete');
+ if (abs(rho) >= 1.0)
+        disp("error: rho must lie between -1 and 1");
+    return
+ end
+ warning('on','all')
+ 
+% % calculation of the covariance matrix
+% covxy = rho*xsd*ysd;
+% scaleCov = .75; % scale cov by some factor
+% C = [xvar covxy; covxy yvar].*scaleCov; % the covariance matrix
+% A = inv(C); % the inverse covariance matrix
+
+w = 1000;
+C = [w 0; 0 w];
+A = inv(C);
+
+xmu = 10; ymu = 10;
+
+
+% Compute value of Gaussian pdf at each point in the grid
+clear zMapped
+% z = (1/sqrt(2*pi*det(C))) * exp(-1/2* (A(1,1)*(X-xmu).^2 + 2*A(1,2)*(X-xmu).*(Y-ymu) + A(2,2)*(Y-ymu).^2));
+z = 10.* exp(-1/2* (A(1,1)*(X-xmu).^2 + 2*A(1,2)*(X-xmu).*(Y-ymu) + A(2,2)*(Y-ymu).^2));
+
+figure
+imagesc(z); colorbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%
+% A: amplitude
+% x0, y0: center 
+% sigma_x, sigma_y: x and y spreads of the blob
+y0 = 10; x0=10;
+sigmaX=5; sigmaY=10;
+A = 10; % firing rate (Hz)
+fxy = A.*exp(-(((X-x0).^2)./(2*sigmaX.^2) + ((Y-y0).^2.)/(2*sigmaY.^2)));
+
+figure
+surf(fxy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
