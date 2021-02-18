cef6b0f2f76ca6acdd58c4e15c54b385ce53532d
Jo Carpenter <jordannc@stud.ntnu.no>
2021-02-18 12:18:55 -0500

    egocentric rm plot
    barebones egocentric ratemap plot
    0 deg is IN FRONT
    90 deg is LEFT
    270 deg is RIGHT
    180 deg is BEHIND

---
+++ rateMaps/plot_egoRM.m
@@ -0,0 +1,33 @@
+function out = plot_egoRM(P, ST, RP)
+%EGORM 
+%   INPUTS-
+%   P:      position vector
+%   ST:     spiketimes
+%   RP:     reference point
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%s
+
+ego_RM = egoRateMap(P, ST, RP);
+[Xi,Yi,Zi,Ci] = polarplot3d(ego_RM, 'PlotType','off');
+
+fig = surf (Xi,Yi,Zi,Ci,'LineStyle','none'); hold on;
+set(gcf,'color','w');
+set(gca, 'visible', 'off', 'box', 'off');
+view(2);
+camroll(90);
+pbaspect([1 1 1]);
+grid off
+shading interp
+
+% find peak firing rate
+maxfr = nanmax(Ci(:));
+[row, col] = find(ismember(Ci, maxfr));
+locxy = [Xi(row,col), Yi(row,col), Zi(row,col)];
+% scatter3(locxy(1), locxy(2), locxy(3), [30], 'k', 'filled');
+
+% package output
+out.fig = fig;
+out.maxfr = maxfr;
+out.loc = locxy;
+
+end
+
