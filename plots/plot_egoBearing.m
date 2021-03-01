4cf0a395f8bb67bff07eb8a29158749199a49bbb
Jo Carpenter <jordannc@stud.ntnu.no>
2021-03-01 09:16:24 -0500

    egocentric bearing plot fn

--- plots/plot_egoBearing.m
+++ plots/plot_egoBearing.m
@@ -1,74 +1,71 @@
-function [tcVals_egoAng, binCtrs_egoAng] = plot_egoBearing(position, ST, refLoc, doPlot)
+function [tc, ctrs] = plot_egoBearing(P, ST, rp, doPlot)
 %PLOT_EGOBEARING Thresholded!
 % October 22, 2020
 % This function can probably replace the other one (egoBearing.m)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% calculate head direction
-[head_direction] = get_hd(position); % in deg
+% unpack inputs
+t = P(:,1); tpf = mode(diff(t));
+X = P(:,2); Y = P(:,3);
+rlX = rp(1); rlY = rp(2);
 
-% Grab window limits for pos tracking
-expansionFactor = 5;
-xMin = nanmin(position(:,2))-expansionFactor;
-xMax = nanmax(position(:,2))+expansionFactor;
-yMin = nanmin(position(:,3))-expansionFactor;
-yMax = nanmax(position(:,2))+expansionFactor;
+% head direction
+[HD] = get_hd(P); % in deg (0 is 'up')
 
-% find point between 2 LEDs
-t = position(:,1); 
-sampleRate = mode(diff(t));
-x1 = position(:,2); y1 = position(:,3);
-x2 = position(:,4); y2 = position(:,5);
-midX = (x1+x2)/2;
-midY = (y1+y2)/2;
+% allo & egocentric bearing
+allo = mod(atan2d(rlY-Y, rlX-X), 360);
+allo = mod(allo + 90, 360); % shift so that 0 deg is in front of animal
+ego = mod(allo - HD, 360);
 
-% break down 'refLoc'
-rlX = refLoc(1,1);
-rlY = refLoc(1,2);
+% spiketrain
+startT = t(1); stopT = t(end);
+ST = ST(ST < stopT & ST > startT);
+time_edges = linspace(startT,stopT,numel(t)+1);
+spiketrain = histcounts(ST, time_edges);
 
-% compute egocentric bearing
-alloAng = atan2d(rlY-midY, rlX-midX)+180;
-egoAng = alloAng - head_direction;
-neg_idx = find(egoAng<0);
-egoAng(neg_idx) = egoAng(neg_idx)+360;
+% instantaneous fr at each timepoint
+inst_fr = spiketrain./tpf;
 
-%% apply speed threshold
+%% apply speed threshold 
+% @todo: make this an option
 threshold = 5; % cm/s
-[P_logical, ST_thresh] = speed_threshold(position, ST, threshold);
+[P_logical, ST_thresh] = speed_threshold(P, ST, threshold);
 
 % find time indices when cell spikes
-idx = knnsearch(t, ST_thresh);
-spkX = x1(idx); spkY = y2(idx);
-spk_egoAng = egoAng(idx);
+spkidx = knnsearch(t, ST_thresh);
+spk_ego = ego(spkidx);
 
 %% compute tuning curve
 nBins = 40; % 9 degree bins
 angEdges = linspace(0,360,nBins);
-[spkEgoAngMap, mapAxis_EgoAng] = histcounts(spk_egoAng,angEdges);
+[bincounts, edges, bin] = histcounts(spk_ego,angEdges);
 
-% make the allEgoAngMap
-for bin = 1:length(angEdges)-1
-    in_this_bin = find(egoAng>angEdges(bin) & egoAng<=angEdges(bin+1));
-    allEgoAngMap(1,bin) = sum(P_logical(in_this_bin));
+% find standard deviation in each bin
+for binNum = 1:length(edges)-1
+    stdev_spk(binNum) = std(inst_fr(find(bin==binNum)), 'omitnan');
 end
-
-for i = 1:length(mapAxis_EgoAng)
-    if i+1 <= length(mapAxis_EgoAng)
-        binCtrs_egoAng(i) = ((mapAxis_EgoAng(i+1)-mapAxis_EgoAng(i))/2)+mapAxis_EgoAng(i);
+% calculate occupancy map
+for bin = 1:length(angEdges)-1
+    idx_now = find(ego>angEdges(bin) & ego<=angEdges(bin+1));
+    stdev_bin(bin) = std(inst_fr(idx_now), 'omitnan');
+    ego_occ(bin) = sum(P_logical(idx_now)); % occupancy (counts)
+end
+for i = 1:length(edges)
+    if i+1 <= length(edges)
+        ctrs(i) = ((edges(i+1)-edges(i))/2)+edges(i);
     end
 end
-tcVals_egoAng = spkEgoAngMap./(allEgoAngMap*sampleRate + eps); 
-
-% smooth tuning curve values
-tcVals_egoAng = imgaussfilt(tcVals_egoAng, 2, 'Padding', 'circular');
+% tuning curve
+tc = bincounts./(ego_occ*tpf + eps); 
+tc = imgaussfilt(tc, 2, 'Padding', 'circular');
 
 %% plot
 
 switch doPlot
     case "True"
-%         figure
         hold on;
-        plot(binCtrs_egoAng, tcVals_egoAng, 'Color', [0 .4 .9], 'LineWidth', 1.10)
-%         title("Egocentric Angle", "FontName", "Calibri Light", "FontSize", 20)
+        plot(ctrs, tc, 'Color', [0 .4 .9], 'LineWidth', 1.10)
+%         errorbar(tc,stdev_spk);
         ylabel("fr (Hz)"); xlabel("angle (deg)");
         xticks([90 180 270 360])
         box off
